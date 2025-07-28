1aa7e118f45ba01e523b0f189ec7047b0e1af3f9
Naufal <diki.naufal29dev@gmail.com>
2025-07-28 13:55:41 +0700

    update code

--- lib/hono.ts
+++ lib/hono.ts
@@ -1,5 +1,27 @@
-import { hc } from "hono/client";
+import { getAuth } from '@hono/clerk-auth';
+import { clerkMiddleware } from '@hono/clerk-auth';
+import { hc } from 'hono/client';
+import { createMiddleware } from 'hono/factory';
+import { HTTPException } from 'hono/http-exception';
 
-import { AppType } from "@/app/api/[[...route]]/route";
+import { AppType } from '@/app/api/[[...route]]/route';
 
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL!);
+
+type AuthEnv = {
+  Variables: {
+    auth: ReturnType<typeof getAuth>;
+  };
+};
+
+export const authedMiddleware = createMiddleware<AuthEnv>(async (c, next) => {
+  const auth = getAuth(c);
+  if (!auth?.userId) {
+    throw new HTTPException(401, {
+      res: c.json({ error: 'Unauthorized.' }, 401),
+    });
+  }
+  await next();
+});
+
+export const clerkMiddlewareWithAuth = [clerkMiddleware(), authedMiddleware];
