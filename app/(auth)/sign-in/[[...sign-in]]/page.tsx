e9c8c8e4d06d56b5311a45177ab6cc6ce59af6c8
Naufal <diki.naufal29dev@gmail.com>
2025-07-28 11:41:47 +0700

    refactoring code

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,29 +1,24 @@
-import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-import { Loader2 } from "lucide-react";
-import Image from "next/image";
+import { SignIn, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 
 const SignInPage = () => {
   return (
     <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
-      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
-        <div className="space-y-4 pt-16 text-center">
+      <div className="flex h-full flex-col items-center justify-center gap-y-8 px-4">
+        <div className="space-y-4 text-center">
           <h1 className="text-3xl font-bold text-[#2E2A47]">Selamat Datang!</h1>
           <p className="text-base text-[#7E8CA0]">
-            Masuk atau buat akun untuk kembali ke dashbor.
+            Masuk atau buat akun untuk kembali ke dasbor.
           </p>
         </div>
-
-        <div className="mt-8 flex items-center justify-center">
-          <ClerkLoaded>
-            <SignIn path="/sign-in" />
-          </ClerkLoaded>
-
-          <ClerkLoading>
-            <Loader2 className="animate-spin text-muted-foreground" />
-          </ClerkLoading>
-        </div>
+        <ClerkLoaded>
+          <SignIn path="/sign-in" />
+        </ClerkLoaded>
+        <ClerkLoading>
+          <Loader2 className="animate-spin text-muted-foreground" />
+        </ClerkLoading>
       </div>
-
       <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
         <Image src="/logo.svg" alt="Logo" height={100} width={100} />
       </div>
